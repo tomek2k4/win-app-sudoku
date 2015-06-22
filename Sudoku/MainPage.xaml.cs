97ec02886adda5a36c518153ebd27881e3e3a7ff
tomek2k4 <tomasz.maslon@gmail.com>
2015-06-22 21:15:58 +0200

    Added viewModel class

--- Sudoku/MainPage.xaml.cs
+++ Sudoku/MainPage.xaml.cs
@@ -13,18 +13,55 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
+using Sudoku.ViewModel;
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Sudoku
 {
+
+    
+
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
     {
+
+        private ViewModelClass _viewModel;
+
         public MainPage()
         {
             this.InitializeComponent();
         }
+
+        #region . Properties: Public .
+
+        /// <summary>
+        /// Gets or sets the ViewModel pointer for this class.
+        /// </summary>
+        internal ViewModelClass ViewModel
+        {
+            get
+            {
+                return _viewModel;
+            }
+            set
+            {
+                _viewModel = value;                             // Save a pointer to the ViewModel class
+                this.DataContext = value;                       // Set the datacontext of the WPF form to the view model.
+            }
+        }
+
+        #endregion
+
+
+
     }
+
+
+
+
+
 }
